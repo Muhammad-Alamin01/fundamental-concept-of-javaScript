8df37a5f700236503f2e2828503497f8d4f2822a
iqbalhossain <iqbalhossainsha2303@gmail.com>
2021-05-08 01:48:45 +0600

    add IIFE

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * function
 * switch
 * object
+* IIFE
+* 
---
+++ iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression (IIFE)
+(function() {
+    const name = 'Sneha';
+    console.log(name);
+
+})();
+
+// function expression
+const myToDo = function(tasks) {
+    console.log(tasks);
+    
+}
